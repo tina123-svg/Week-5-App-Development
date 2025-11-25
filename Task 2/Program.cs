fbb856ba2b8cd2799e659666d003f3e079321bbc
tina123-svg <tinasitaula835@gmail.com>
2025-11-25 12:03:43 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating object of Car
+        Car car = new Car();
+        car.Brand = "Toyota";
+        car.Speed = 120;
+        car.Seats = 5;
+
+        // Creating object of Motorcycle
+        Motorcycle bike = new Motorcycle();
+        bike.Brand = "Yamaha";
+        bike.Speed = 90;
+        bike.HasCarrier = true;
+
+        // Calling methods from base class
+        car.Start();
+        car.DisplayInfo();
+        car.Stop();
+
+        Console.WriteLine();
+
+        bike.Start();
+        bike.DisplayInfo();
+        bike.Stop();
+    }
+}
---
+++ Task 2/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿public class Vehicle
+{
+    public string Brand { get; set; }
+    public int Speed { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine($"{Brand} is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"{Brand} is stopping...");
+    }
+
+    // Virtual method (so child classes can override)
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h");
+    }
+}
+
+// Derived Class 1
+public class Car : Vehicle
+{
+    public int Seats { get; set; }  // Unique field
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h, Seats: {Seats}");
+    }
+}
+
+// Derived Class 2
+public class Motorcycle : Vehicle
+{
+    public bool HasCarrier { get; set; }  // Unique field
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h, Carrier: {HasCarrier}");
+    }
+}
